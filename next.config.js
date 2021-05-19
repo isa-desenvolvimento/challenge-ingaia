e3ef0ab8cba28b1359167be26c8a55eb66d29ca4
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-19 00:21:37 -0300

    fix: ci error

--- next.config.js
+++ next.config.js
@@ -1,27 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      mozjpeg: {
-        quality: 80
-      },
-      pngquant: {
-        speed: 3,
-        strip: true,
-        verbose: true
-      },
-      imagesPublicPath: '/challenge-ingaia/_next/static/images/'
-    }
-  ],
-  {
-    i18n: {
-      locales: ['pt-BR', 'en-US'],
-      defaultLocale: 'pt-BR'
-    },
-    basePath: '/challenge-ingaia',
-    assetPrefix: '/challenge-ingaia/'
-  }
-])
+module.exports = {
+  i18n: {
+    locales: ['pt-BR', 'en-US'],
+    defaultLocale: 'pt-BR'
+  },
+  basePath: '/challenge-ingaia',
+  assetPrefix: '/challenge-ingaia/'
+}
