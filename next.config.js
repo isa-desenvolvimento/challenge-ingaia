5a657a3e7a6580e37fc77252c0c33b0931eaf0c8
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-18 23:43:58 -0300

    feat: add travis e pageGitHub

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-module.exports = {
-  i18n: {
-    locales: ['pt-BR', 'en-US'],
-    defaultLocale: 'pt-BR'
+import withPlugins from 'next-compose-plugins'
+import optimizedImages from 'optimized-images'
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true
+      },
+      imagesPublicPath: '/challenge-ingaia/_next/static/images/'
+    }
+  ],
+  {
+    i18n: {
+      locales: ['pt-BR', 'en-US'],
+      defaultLocale: 'pt-BR'
+    },
+    basePath: '/challenge-ingaia',
+    assetPrefix: '/challenge-ingaia/'
   }
-}
+])
