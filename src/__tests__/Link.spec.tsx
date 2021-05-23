dc3210767351374911bcbdd1049c55f2ec457319
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-23 18:42:46 -0300

    test: link

--- src/__tests__/Link.spec.tsx
+++ src/__tests__/Link.spec.tsx
@@ -1,22 +1,18 @@
-import { render } from '@testing-library/react';
-import Link from '../components/Link';
+import { renderWithProvider } from '../utils/helpers'
+import { screen } from '@testing-library/react'
+
+import Link from '../components/Link'
 
 describe('<Link />', () => {
-  it('should render component Link children', () => {
-    const { getByRole } = render(
-      <Link href="/">
-        <h1>Test Link</h1>
-      </Link>,
-    );
-    expect(getByRole('heading', { name: /test Link/i })).toBeInTheDocument();
-  });
+  renderWithProvider(
+    <Link href="/">
+      <h1>Test Link</h1>
+    </Link>
+  )
 
-  it('should render snapshot', () => {
-    const { container } = render(
-      <Link href="/">
-        <h1>Test Link</h1>
-      </Link>,
-    );
-    expect(container).toMatchSnapshot();
-  });
-});
+  it('should render component Link children', () => {
+    expect(
+      screen.getByRole('heading', { name: /test Link/i })
+    ).toBeInTheDocument()
+  })
+})
