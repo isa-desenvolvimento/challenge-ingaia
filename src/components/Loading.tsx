1d8d2762437f23944d1663f62ca19fff098269c4
Andressa Novaes <isa.menezes@ilia.digital>
2021-05-23 20:04:24 -0300

    test: loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,17 +1,18 @@
-import { useTranslationContext } from 'locales/translationContext'
-import * as S from 'styles/Loading'
+import { useTranslationContext } from '../locales/translationContext'
+import * as S from '../styles/Loading'
 import Image from 'next/image'
 export default function Loading() {
   const translation = useTranslationContext()
 
   return (
-    <S.Container>
+    <S.Container role="loading">
       <S.ImageContainer>
         <Image
           src="/assets/background-loading.png"
           width="183"
           height="183"
           alt={translation.ALT_LOADING}
+          role="image"
         />
         <S.Text>{translation.LOADING}</S.Text>
       </S.ImageContainer>
